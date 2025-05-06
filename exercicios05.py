812ac551f43f539ab30204e58be9c468f2d7ec96
caminhosheitor <00001107322145sp@al.educacao.sp.gov.br>
2025-05-06 12:07:04 -0300

    fizemos uma lista pra cadastro de aluno

--- exercicios05.py
+++ exercicios05.py
@@ -1,12 +1,19 @@
-def cadastrar_aluno(nome, email, serie):
+def cadastrar_aluno(nome, email, serie,  nota01, nota02, nota03):
     alunos = []
 
     aluno = {
         "nome": nome,
        "email": email,
        "serie": serie,
+       "nota": [nota01, nota02, nota03]
     }
     alunos.append(aluno)
     return alunos 
     
-print("cadastrar_aluno"("caminhão", "00001107322145", "2ºTB"))
+print(cadastrar_aluno("caminhão", "00001107322145", "2ºTB", 7, 9, 6))
+
+
+
+
+
+
