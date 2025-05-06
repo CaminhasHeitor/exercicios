60b84dd848c71a9c4777061b7001646704945455
caminhosheitor <00001107322145sp@al.educacao.sp.gov.br>
2025-05-06 08:11:20 -0300

    p[nesse codigo fizemos somas de diferentes numeros

---
+++ exercicios01.py
@@ -0,0 +1,19 @@
+def somar_pares(lista):
+    total = 0 
+    for i in lista:
+           total += i 
+    return total
+
+numeros = [2, 738, 69, 33, 17, 20]
+
+print(somar_pares(numeros)) 
+
+
+
+
+
+
+
+
+
+
