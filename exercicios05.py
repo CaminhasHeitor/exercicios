a644b0d672cb233d2e7a45013a375f42e2f71a12
caminhosheitor <00001107322145sp@al.educacao.sp.gov.br>
2025-05-12 14:18:21 -0300

    criamos a media dentro da função, e configuramos a notas com o valor zero

--- exercicios05.py
+++ exercicios05.py
@@ -1,16 +1,22 @@
+from exercicios04 import somar_pares
+
 def cadastrar_aluno(nome, email, serie,  nota01, nota02, nota03):
     alunos = []
+
+    notas = [nota01, nota02, nota03]
 
     aluno = {
         "nome": nome,
        "email": email,
        "serie": serie,
-       "nota": [nota01, nota02, nota03]
+       "nota": [nota01, nota02, nota03],
+       "media": somar_pares(notas)
     }
     alunos.append(aluno)
+    
     return alunos 
     
-print(cadastrar_aluno("caminhão", "00001107322145", "2ºTB", 7, 9, 6))
+print(cadastrar_aluno("caminhão", "00001107322145", "2ºTB"))
 
 
 
