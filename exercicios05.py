f3685756e277b29fb4739e9b29afe3ec21554271
caminhosheitor <00001107322145sp@al.educacao.sp.gov.br>
2025-05-13 11:50:41 -0300

    criamos

--- exercicios05.py
+++ exercicios05.py
@@ -1,7 +1,17 @@
 from exercicios04 import somar_pares
+ 
+alunos = []
 
-def cadastrar_aluno(nome, email, serie,  nota01, nota02, nota03):
-    alunos = []
+def obter_dados_aluno():
+    nome_aluno = input("informe o nome do aluno: ")
+    email_aluno = input("informe o email do aluno: ")
+    serie_aluno = input("informe a serie do aluno: ")
+    nota01 = int(input("informe a primeira nota do aluno: "))
+    nota02 = int(input("informe a segunda nota do aluno: "))
+    nota03 = int(input("informe a terceira nota do aluno: "))
+
+    return cadastrar_aluno(nome_aluno, email_aluno, serie_aluno, nota01, nota02, nota03)
+def cadastrar_aluno(nome, email, serie,  nota01=0, nota02=0, nota03=0):
 
     notas = [nota01, nota02, nota03]
 
@@ -16,7 +26,34 @@ def cadastrar_aluno(nome, email, serie,  nota01, nota02, nota03):
     
     return alunos 
     
-print(cadastrar_aluno("caminhão", "00001107322145", "2ºTB"))
+
+
+def mostrar_dados_alunos(dados_alunos):
+    for aluno in dados_alunos:
+            print(f"Nome do Aluno: {aluno["nome"]} | Email do aluno: {aluno["email"]}  | Série do aluno:{aluno["serie"]} | Notas do aluno: {aluno["notas"]} | Média do aluno: {aluno["media "]}")
+
+    return 
+
+
+def iniciar_sistema():
+    while True:
+        print("="*80)
+        print("Opção 1 => mostrar alunos cadastrados.")
+        print("Opção 2 => cadastrar alunos.")
+        print("Opção 3 => sair do sistema.")
+        print("="*80)
+        
+        opcao = input("Escolha uma das opcções acima: ")
+
+        if opcao == "1":
+            mostrar_dados_alunos(alunos)
+        elif opcao == "2":
+            obter_dados_aluno()
+        else:
+            print("sistema finalizado.")
+            break
+
+iniciar_sistema()
 
 
 
